3836873758aedb051fdca1f3fe92a6ac9448b8a9
Chanyub.park <mrchypark@gmail.com>
2018-05-17 01:40:56 +0900

    ing ubci_get

--- R/ubci_get.R
+++ R/ubci_get.R
@@ -1,7 +1,25 @@
 #' get ubci
 #'
+#' get ubci data.
+#'
+#' @param index default is UBMI.
+#' @param count default is 10000.
+#'
 #' @export
+#' @importFrom httr GET content
+#' @importFrom tibble as_tibble
 
-ubci_get <- function(){
-  return("hi")
+ubci_get <- function(index = "UBMI", count = 10000) {
+  tar <-
+    paste0(
+      "https://crix-api-cdn.upbit.com/v1/crix/candles/days?code=IDX.UPBIT.",
+      index,
+      "&count=",
+      count
+    )
+  res <- httr::GET(tar)
+  jsonReturn <- httr::content(res)
+  dat <- do.call(rbind, jsonReturn)
+  # dat <- data.frame(apply(dat, 2, unlist),stringsAsFactors = F)as
+  return(dat)
 }
